a8ee4edf7b42bfd2cb2440e8e2a787ef1a84b337
BasicallyWiz <48531736+BasicallyWiz@users.noreply.github.com>
2023-03-14 01:31:12 -0700

    Dev things! No touchies!
    Added:
    - Dev command, this will be used as a command group for all commands that should only be useable by developers.
    
    Changed:
    - Moved ArchiveGogs into Dev, because it's a dev only command.

---
+++ WizBotLibrary/Commands/Dev.cs
@@ -0,0 +1,77 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WizBotLibrary.Commands.Interfaces;
+using WizBotLibrary.Modules.DiscordNetExtensions;
+
+namespace WizBotLibrary.Commands {
+  public class Dev : ISlashCommand {
+    public SlashCommandBuilder Builder 
+    {
+      get {
+        SlashCommandBuilder builder = new SlashCommandBuilder();
+        builder.Name = nameof(Dev).ToLower();
+        builder.Description = "Developer commands. If you're not a dev, this command won't do what you want.";
+
+        var option = new SlashCommandOptionBuilder();
+        option.Name = "indexgogs";
+        option.Description = "Collects gogs, and indexes them.";
+        option.Type = ApplicationCommandOptionType.SubCommand;
+        builder.AddOption(option);
+
+        return builder;
+      }
+    }
+
+    public async Task Execute(SocketSlashCommand inputCommand, WizBot Bot) {
+      await Bot.logger.Debug(inputCommand.Data.ToStringFormatted());
+
+      switch (inputCommand.Data.Options.ElementAt(0).Name) {
+        case "indexgogs": 
+        {
+          await inputCommand.DeferAsync();
+
+          IMessageChannel SourceChannel;
+          if (Bot.IsDebugMode) { // This section executes in debug mode
+            SourceChannel = Bot.client.GetChannel(780649979039580170) as IMessageChannel;
+          }
+          else {  //  This section executes in release mode
+            SourceChannel = Bot.client.GetChannel(831612912266641408) as IMessageChannel;
+          }
+
+          if (SourceChannel == null) { await inputCommand.FollowupAsync("We can't reach the channel that's meant to be archived."); return; }
+          List<string> UrlList = new List<string>();
+
+          //  Executes regardless of debug or release
+          var messagesEnumerable = SourceChannel.GetMessagesAsync(1000, CacheMode.AllowDownload).Flatten();
+          await messagesEnumerable.ForEachAwaitAsync((IMessage message) => {
+            if (message.Attachments.Count > 0) {
+              foreach (IAttachment attachment in message.Attachments) {
+                UrlList.Add(attachment.Url);
+              }
+            }
+            else {
+              UrlList.Add(message.Content);
+            }
+
+            return Task.CompletedTask;
+          });
+
+          if (!Directory.Exists($"{Directory.GetCurrentDirectory()}{OSTools.DirSep}gogs{OSTools.DirSep}")) {
+            Directory.CreateDirectory($"{Directory.GetCurrentDirectory()}{OSTools.DirSep}gogs{OSTools.DirSep}");
+          }
+
+          File.WriteAllLines($"{Directory.GetCurrentDirectory()}{OSTools.DirSep}gogs{OSTools.DirSep}gogs.txt", UrlList.ToArray().Reverse());
+          await Bot.logger.Info($"Archived gogs to: {Directory.GetCurrentDirectory()}{OSTools.DirSep}gogs{OSTools.DirSep}");
+          await inputCommand.FollowupAsync("Finished indexing gog Urls.");
+        } break;
+
+      }
+    }
+  }
+}
---
+++ WizBotLibrary/Modules/DiscordNetExtensions/SocketSlashCommandData.cs
@@ -0,0 +1,32 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizBotLibrary.Modules.DiscordNetExtensions {
+  public static class SocketSlashCommandDataExtension {
+    public static string ToStringFormatted(this SocketSlashCommandData data) {
+
+      string TheString = "";
+      TheString += $"{data.Id}\n";
+      TheString += $"{data.Name}\n";
+      foreach(SocketSlashCommandDataOption option in data.Options) {
+        TheString += $"{option.ToStringFormatted()}\n";
+      }
+
+      return TheString;
+    }
+
+    public static string ToStringFormatted(this SocketSlashCommandDataOption option) {
+      string TheString = "";
+      TheString += $"{option.Name}";
+      foreach (SocketSlashCommandDataOption Option in option.Options) {
+        TheString += $"Option.ToStringFormatted()\n";
+      }
+
+      return TheString;
+    }
+  }
+}
